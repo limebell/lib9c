71e76df42eec7a309c8e7fc78efba2009aaf7047
Chanhyuck Ko <limeelbee@gmail.com>
2023-10-04 15:26:54 +0900

    temp

--- Lib9c/Module/AccountHelper.cs
+++ Lib9c/Module/AccountHelper.cs
@@ -2,6 +2,7 @@
 using Bencodex.Types;
 using Libplanet.Action.State;
 using Libplanet.Crypto;
+using Libplanet.Store.Trie;
 
 namespace Nekoyume.Module
 {
@@ -18,14 +19,14 @@ namespace Nekoyume.Module
         /// If not, legacy account.</returns>
         public static IValue? Resolve(IWorldState world, Address address, Address accountAddress)
         {
-            if (world.Legacy)
+            IAccount account = world.GetAccount(accountAddress);
+            if (account.Trie.Hash.Equals(MerkleTrie.EmptyRootHash))
             {
                 return world.GetAccount(ReservedAddresses.LegacyAccount).GetState(address);
             }
-            else
-            {
-                return world.GetAccount(accountAddress).GetState(address);
-            }            
+
+            IValue? state = account.GetState(address);
+            return state ?? world.GetAccount(ReservedAddresses.LegacyAccount).GetState(address);
         }
     }
 }
